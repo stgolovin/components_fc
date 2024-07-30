dd1ec4c503e0f132316f12e6a6b74ddac08c21b0
Stanislav Golovin <golovin.ds93@gmail.com>
2024-07-30 12:29:00 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+import { ShopItemFunc } from './components/ShopItemFunc'
 import './App.css';
 
+
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="container">
+          <div className="background-element">
+          </div>
+          <div className="highlight-window">
+            <div className="highlight-overlay"></div>
+          </div>
+          <div className="window">
+            <ShopItemFunc item={ ShopItemFunc.item}/>
+          </div>
+        </div>
+      );
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders LEONARD COAT', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const nameElement = screen.getByText(/LEONARD COAT/i);
+  expect(nameElement).toBeInTheDocument();
 });
